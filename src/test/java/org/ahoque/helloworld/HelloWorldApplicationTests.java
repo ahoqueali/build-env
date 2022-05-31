bda79ccfb422d7dbe1468c3607e42c7ac9627217
Abdul Hoque Ali <abdali@WKMZT2E5494D.global.publicisgroupe.net>
2022-05-31 12:36:58 +0100

    Add spring boot app

---
+++ src/test/java/org/ahoque/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ahoque.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
